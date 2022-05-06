eb65cd4ccb84a939ba54fa52d64ccf0a4719c638
Muniraja <muniraja0799@gmail.com>
2022-05-06 21:54:34 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./home/Home";
+
+const App = () => {
+  return (
+    <div className="home">
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section id="home">
+      <div className="container">
+        <h2>Hello Muniraja, Welcome to Homepage</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
